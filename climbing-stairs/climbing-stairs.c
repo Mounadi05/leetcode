7d9d9ec4a8d51365782eb963627506cf000bcbbf
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-13 08:50:48 +0000

    Delete climbing-stairs directory

--- climbing-stairs/climbing-stairs.c
+++
@@ -1,8 +0,0 @@
-int tab[46] = {0};
-int climbStairs(int n){
-    if (n <= 3)
-        return (n);
-    if (tab[n] != 0)
-        return (tab[n]);
-    return tab[n] = climbStairs(n - 1) + climbStairs(n - 2);
-}
