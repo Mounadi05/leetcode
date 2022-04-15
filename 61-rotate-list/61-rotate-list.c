e421d21ce01f01db517c48444c4cd547557471bf
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-15 07:35:56 +0000

    Time: 4 ms (72.95%), Space: 6.1 MB (77.84%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* rotateRight(struct ListNode* head, int k){  
+    struct ListNode* tmp = head;
+    if (!head)
+        return (head);
+    int i = 0;
+    while(tmp && ++i)
+        tmp = tmp->next;
+    if (k > i)
+        k = k%i;
+    tmp = head;
+    struct ListNode* tmp1 = NULL;
+    while (--k >= 0)
+    {
+            while(head && head->next)
+            {
+                if (!head->next->next)
+                {
+                    tmp1 = head->next;
+                    head->next = NULL;
+                    tmp1->next = tmp;
+                    tmp = tmp1;
+                }
+                head = head->next;
+            }
+            head = tmp;
+    }
+    return (head);
+}
