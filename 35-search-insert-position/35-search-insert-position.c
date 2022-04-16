5f304242cd9b7fa3a4ab7465072356b1eb4ef216
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-16 05:32:28 +0000

    Time: 3 ms (91.70%), Space: 5.9 MB (99.23%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.c
+++ 35-search-insert-position/35-search-insert-position.c
@@ -1,14 +1,10 @@
-
-
-
-int searchInsert(int* num, int size, int t){
+ int searchInsert(int* num, int size, int t){
     int i = size/2;
-    int a = 0;
     if (num[size - 1] < t)
         return (size);
     while(i < size - 1 && i > 0 )
     {
-        if(num[i] == t || ( num[i-1] < t && num[i] > t))
+        if(num[i] == t || (num[i-1] < t && num[i] > t))
             return (i);
         else if (num[i] < t)
             i++;
