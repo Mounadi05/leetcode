422f4dbebaf8d466fb20ed68203e01b21c1643eb
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-17 07:19:59 +0000

    Time: 8 ms (74.35%), Space: 8.9 MB (38.04%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int check(struct TreeNode* root,long min , long max)
+{
+    if (!root)
+        return 1;
+    if (root->val <= min || root->val >= max)
+        return (0);
+    return ( check(root->left, min ,root->val) && check(root->right ,root->val,max));
+}
+bool isValidBST(struct TreeNode* root){
+    struct TreeNode* r;
+    long max = 4294967295;
+    long  min = -4294967296;
+    if (check(root, min,max))
+        return (true);
+    return (false);
+}
