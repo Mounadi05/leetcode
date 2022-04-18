7db56c9719b0083d3a13495aad77c2ff8f1fee82
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-18 21:31:19 +0000

    Time: 3 ms (38.91%), Space: 5.7 MB (39.70%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.c
@@ -0,0 +1,22 @@
+bool isValid(char * s){
+	int i = 0;
+	int a = 0;
+    char *stack = malloc(strlen(s) + 1);
+	while (s[i])
+	{
+		if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+			stack[a++] = s[i];
+        else if(a && s[i] == ']' && stack[a - 1] == '[')
+			stack[a--];
+        else if(a && s[i] == ')' && stack[a - 1] == '(')
+			stack[a--];
+        else if(a && s[i] == '}' && stack[a - 1] == '{')
+			stack[a--];
+        else
+            return (false);
+		i++;
+	}
+	if (a != 0)
+        return (false);
+    return (true);
+}
