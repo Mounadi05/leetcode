2e6bbd42fb4d290fbfc12e5cb9644a481e66e6f9
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-19 05:08:15 +0000

    Time: 3 ms (36.65%), Space: 5.8 MB (15.53%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.c
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.c
@@ -1,39 +1,24 @@
-void    ft_swap(char *a , char *b)
-{
-    char u;
-    u = *a;
-    *a = *b;
-    *b = u;
-}
-
-int check(char a, char b)
-{
-    if (a == b)
-        return (1);
-    return(0);
-}
 bool areAlmostEqual(char * s1, char * s2){
     int len = strlen(s1);
     int c = 0;
-    int i = 0;
+    int i = -1;
     int a = --len;
-    while(i <= a)
+    while(++i < a)
     {
         while(i <= len)
         {
             if (!strcmp(s1,s2))
                 return (true);
-            else if(check(s2[len] ,s1[i]) && s1[i] != s2[i] && s1[len] != s2[len])
+            else if((s2[len] == s1[i]) && s1[i] != s2[i] && s1[len] != s2[len])
             {
                 c++;
-                ft_swap(&s1[len], &s1[i]);
+                (s1[len] ^= s1[i]),(s1[i] ^= s1[len]),(s1[len] ^= s1[i]);
                 if (c > 1)
                     return (false);
             }
             len--;
         }
         len = a;
-        i++;
     }
     if (!strcmp(s1,s2))
             return (true);
