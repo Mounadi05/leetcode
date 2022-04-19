2423f9035555444aec13208e1ffe0e615fd0e150
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-19 07:19:36 +0000

    Time: 0 ms (100.00%), Space: 5.4 MB (68.69%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.c
+++ 70-climbing-stairs/70-climbing-stairs.c
@@ -1,8 +1,14 @@
-int tab[46] = {0};
+/*int tab[46] = {0};
 int climbStairs(int n){
     if (n <= 3)
         return (n);
     if (tab[n] != 0)
         return (tab[n]);
     return tab[n] = climbStairs(n - 1) + climbStairs(n - 2);
+}*/
+int climbStairs(int n){
+    int a1 = 0,a2 = 1;
+    while(--n >= 0)
+        (a2 += a1),(a1 = a2 - a1);
+    return (a2);
 }
