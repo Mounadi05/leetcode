71713a5430ce5e53a258d02f306ddec16938cf85
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-21 04:03:10 +0000

    Time: 3 ms (45.45%), Space: 5.6 MB (45.45%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.c
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.c
@@ -7,11 +7,8 @@ int check_zero(int n)
 {
     int len = 1;
     int a = n;
-    while (n > 9)
-    {
-        len++;
+    while (n > 9 && len++)
         n /=10;
-    }
     char *tab= malloc(len + 1);
     sprintf(tab, "%d",a);
     a = 0;
@@ -21,26 +18,15 @@ int check_zero(int n)
     return (1);
 }
 int* getNoZeroIntegers(int n, int *size){
-    int *tab = malloc(4000);
-    int a = 1;
-    int i = 0;
-    int d = 0;
-    int c = 0;
-    while (a <= n)
-    {
-        if (check_zero(a))
+    int *tab = malloc(8);
+    int a = 0;
+    while (++a <= n)
+        if (check_zero(a) && check_zero(n - a))
         {
-            c = a;
-            d = n - c;
-            if (check_zero(d) && n == c + d)
-            {
-                tab[0] = c;
-                tab[1] = d;
+                tab[0] = a;
+                tab[1] = n-a;
                 break;
-            }
         }
-        a++;
-    }
     *size = 2;
     return (tab);
 }
