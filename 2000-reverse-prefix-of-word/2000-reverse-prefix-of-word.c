6ca1cc67db067a1eb7f7fa625e55fa3f8fe03308
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-22 01:15:24 +0000

    Time: 0 ms (100.00%), Space: 5.7 MB (63.16%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.c
@@ -0,0 +1,29 @@
+
+
+char * reversePrefix(char * word, char ch){
+    int i = 0;
+    char *str = malloc(strlen(word) + 1);
+    int j = 0;
+    while(word[i])
+    {
+        if (word[i] == ch)
+        {
+            j = i;
+            i = 0;
+            while (j >= 0)
+            {
+                str[i++] = word[j--];
+            }
+            while(word[i])
+            {
+                str[i] = word[i];
+                i++;
+            }
+            str[i] = '\0';
+            return (str);
+        }
+        i++;
+    }
+    return (word);
+    
+}
