0848ec377b2404b2eac9faad3cf98aaa66df1ea8
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-22 03:29:53 +0000

    Time: 91 ms (16.07%), Space: 12.3 MB (93.22%) - LeetHub

--- 344-reverse-string/344-reverse-string.c
+++ 344-reverse-string/344-reverse-string.c
@@ -1,9 +1,32 @@
-void reverseString(char* s, int size){
+/* ********************************************************************************************** */
+/*                                                                                                */
+/*                                                                       ******     ******        */
+/*   344.c                                              :+:      :+:    :+:   */
+/*                                                                     *  *  *  :+:  *  *  *      */
+/*   By: mounadi05 <mounadi1337@gmail.com>                                                        */
+/*                                                                       *               *        */
+/*   Created: 2021/12/31 20:11:30 by mounadi05                            *   -------   *         */
+/*   Updated: 2021/12/31 20:12:23 by amounadi         ###   ########.fr       */
+/*                                                                                                */
+/* ********************************************************************************************** */
+
+/*void reverseString(char* s, int sSize){
+    int i = 0;
     char u;
+    sSize--;
+    while (i <= sSize)
+    {
+        u = s[sSize];
+        s[sSize] = s[i];
+        s[i] = u;
+        i++;
+        sSize--;
+    }
+}*/
+void reverseString(char* s, int size){
     if (size < 1)
         return ;
-    u = s[0];
-    s[0] = s[size - 1];
-    s[size - 1] = u;
+    if (s[0] != s[size - 1])
+        (s[0] += s[size - 1]),(s[size - 1] = s[0] - s[size- 1]),(s[0] -=s[size - 1]);
     reverseString(s+1, size-2);
 }
