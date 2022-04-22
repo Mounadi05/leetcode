f85752017a3f1a7dad2d63e2369fdbc6e7cf7718
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-22 03:36:12 +0000

    Time: 56 ms (70.75%), Space: 12.3 MB (93.22%) - LeetHub

--- 344-reverse-string/344-reverse-string.c
+++ 344-reverse-string/344-reverse-string.c
@@ -10,23 +10,16 @@
 /*                                                                                                */
 /* ********************************************************************************************** */
 
-/*void reverseString(char* s, int sSize){
-    int i = 0;
-    char u;
-    sSize--;
-    while (i <= sSize)
-    {
-        u = s[sSize];
-        s[sSize] = s[i];
-        s[i] = u;
-        i++;
-        sSize--;
-    }
-}*/
-void reverseString(char* s, int size){
+void reverseString(char* s, int size){  
+    int i = -1;
+    while (++i < size--)
+        if (s[i] != s[size])
+            (s[i] += s[size]),(s[size] = s[i] - s[size]),(s[i] -=s[size]);
+}
+/*void reverseString(char* s, int size){
     if (size < 1)
         return ;
     if (s[0] != s[size - 1])
         (s[0] += s[size - 1]),(s[size - 1] = s[0] - s[size- 1]),(s[0] -=s[size - 1]);
     reverseString(s+1, size-2);
-}
+}*/
