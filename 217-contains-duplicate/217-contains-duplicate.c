2340f2f05b455fdcf22fef14c1121210cb728c42
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-23 00:31:08 +0000

    Time: 127 ms (68.35%), Space: 12.7 MB (75.59%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.c
+++ 217-contains-duplicate/217-contains-duplicate.c
@@ -4,10 +4,9 @@ int cmp( const void *a ,const void *b)
     return ( *(int *)a - *(int *)b );
 }
 bool containsDuplicate(int* num, int size){
-    int i = 0;
    qsort(num, size, sizeof(int), cmp);
-    while(i < size - 1)
-        if (num[i] == num[++i])
+    while(--size)
+        if (num[size] == num[size - 1])
             return (true);
     return (false);
 }
