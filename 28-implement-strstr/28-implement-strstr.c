b9950fab1cd556c0eb62187f1ed1039c52c15b2e
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-25 02:01:17 +0000

    Time: 0 ms (100.00%), Space: 5.7 MB (78.82%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.c
+++ 28-implement-strstr/28-implement-strstr.c
@@ -3,12 +3,11 @@ int strStr(char * haystack, char * needle){
     int i,j = i = -1;
     if (!needle)
         return (0);
-    while(haystack[++i])
+    while(haystack[++i] && (j = -1))
     {
         while(haystack[i + ++j] && needle[j] && (haystack[i + j] == needle[j]));
         if (!needle[j])
             return (i);
-        j = -1;
     }
     return (-1);
 }
