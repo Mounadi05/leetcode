536b760fe65413dca52bddf8f3ed5dcf8689f8f2
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-04-27 02:47:02 +0000

    Time: 24 ms (6.40%), Space: 6.8 MB (23.20%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.c
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.c
@@ -1,12 +1,11 @@
-bool checkStraightLine(int** coords, int coordinatesSize, int* coordinatesColSize){
-    int i = 1;
-    int x1 = 0,x2 = 0,y1 =0 ,y2 =0;
+ bool checkStraightLine(int** coords, int size, int* coordinatesColSize){
+    int x1 = 0,x2 = 0,y1 =0 ,y2 =0, i = 1;
     y1 = coords[1][0] - coords[0][0];
     y2 =coords[1][1] - coords[0][1];
-    while(++i<coordinatesSize)
+    while(++i<size)
     {
         x1 = coords[i][0] - coords[0][0];
-        x2 =coords[i][1] - coords[0][1];
+        x2 = coords[i][1] - coords[0][1];
              if(x1*y2 != x2*y1)
                 return false;
     }
