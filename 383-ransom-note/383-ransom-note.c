51b7c2e9bef42cb954e36b160547a899f2e7c068
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-06-03 05:01:32 +0100

    Time: 15 ms (31.14%), Space: 6.4 MB (60.31%) - LeetHub

--- 383-ransom-note/383-ransom-note.c
+++ 383-ransom-note/383-ransom-note.c
@@ -1,17 +1,10 @@
-
-
-bool canConstruct(char * r, char * m){
+ bool canConstruct(char * r, char * m){
     int i,j;
     j = -1;
     i = 0;
     int len = strlen(m);
     while(++j < len)
         if (r[i] == m[j] && ++i)
-        {
-            m[j] = -2;
-            j = -1;
-        }
-    if (!r[i])
-        return (1);
-    return (0);
+            j = m[j] = -1;
+    return (!r[i]);
 }
