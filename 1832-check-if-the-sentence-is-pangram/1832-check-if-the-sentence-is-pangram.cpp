71e1ef11117a5900e257a509cbb267b5b352e0e0
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-09-08 00:29:05 +0100

    Time: 6 ms (38.16%), Space: 6.6 MB (50.33%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -5,8 +5,7 @@ public:
         for(int i = 0; i < s.size(); i++)
             a[(s[i] - 97)] += 1;
         for(int i = 0; i < 26 ; i++)
-            if (a[i] == 0)
-                return false;
+            if (a[i] == 0)return false;
         return true;
     }
 };
