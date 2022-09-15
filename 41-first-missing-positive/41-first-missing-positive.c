d56c84432fed801c1a706b0babbed071684b0d0e
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-09-15 12:57:37 +0100

    Time: 73 ms (82.80%), Space: 11.7 MB (67.60%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.c
+++ 41-first-missing-positive/41-first-missing-positive.c
@@ -1,15 +1,13 @@
 int firstMissingPositive(int* nums, int size){
     int a = INT_MAX;
     int *n = malloc(4 * size);
- 
-        for(int i = 0; i < size ; i++)
-            n[i] = 0;
-        for(int i = 0; i < size ; i++)
-            if (nums[i] <= size && nums[i] > 0)
-                n[nums[i]-1] = nums[i];
-        for(int i = 0 ; i < size; i++)
-            if (n[i] == 0)
-                return i+1;
-        return size + 1;
-
+    for(int i = 0; i < size ; i++)
+          n[i] = 0;
+    for(int i = 0; i < size ; i++)
+        if (nums[i] <= size && nums[i] > 0)
+            n[nums[i]-1] = nums[i];
+    for(int i = 0 ; i < size; i++)
+        if (n[i] == 0)
+            return i+1;
+    return size + 1;
 }
