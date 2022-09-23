7d7274f7bd9f51b59547d345ce1167422ca879b7
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-09-23 19:21:53 +0100

    Time: 815 ms (23.09%), Space: 6.5 MB (94.33%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.c
@@ -0,0 +1,20 @@
+
+
+int pivotIndex(int* nums,int  size){
+
+    int i = -1;
+    int a = 0;
+    int b = 0;
+    int j;
+    while(++i < size)
+    {
+        j = i;
+        while(++j < size)
+           a += nums[j];
+        if (a == b)
+            return i;
+        b += nums[i];
+        a = 0;
+    }
+    return -1;
+}
