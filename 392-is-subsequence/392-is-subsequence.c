5f3dbe7631b67d9f282ac3eec2b4829b91a9ca40
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-09-24 18:02:39 +0100

    Time: 2 ms (60.12%), Space: 5.5 MB (69.57%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.c
+++ 392-is-subsequence/392-is-subsequence.c
@@ -1,9 +1,6 @@
   
 bool isSubsequence(char * s, char * t){
-    int a = 0;
-    int i = 0;
     for(int j = 0 ; t[j] ; j++)
-        if (s[i] == t[j] && s[i])
-            i++;
-    return !s[i];
+        if (*s && *s == t[j]  && s++);
+    return !*s;
 }
