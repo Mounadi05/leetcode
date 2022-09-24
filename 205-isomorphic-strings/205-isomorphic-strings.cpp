e8fca67af8a713e616a4a8841f4475c76701d009
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-09-24 23:25:27 +0100

    Time: 17 ms (27.84%), Space: 6.7 MB (98.32%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int i = 0;
-        int j = 0;
-     
-        while(i < s.length() && i < t.length())
-        {
-            if(s.find_first_of(s[i],0) != t.find_first_of(t[i],0))
+        int i = -1;    
+        while(++i < s.length() && i < t.length())
+           if(s.find_first_of(s[i],0) != t.find_first_of(t[i],0))
                 return false;
-            i++;
-        }
         return true;
     }
 };
