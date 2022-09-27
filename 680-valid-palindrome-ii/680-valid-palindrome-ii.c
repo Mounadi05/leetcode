60424e55576350879bf9c35edf82309d4ae0bdd3
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2022-09-27 03:05:04 +0100

    Time: 40 ms (17.43%), Space: 9.3 MB (34.86%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.c
@@ -0,0 +1,24 @@
+ bool validPalindrome(char * s){
+    int i = -1;
+    int a = 0;
+    int b = 0;
+    int len = strlen(s);
+    while(++i < --len)
+    {
+        if (s[i] != s[len] && a == 1 && (b = 1))
+            break;
+        if (s[i] != s[len] && ++a)
+            i--;
+    }
+     i = -1;
+     len = strlen(s);
+     a = 0;
+    while(++i < --len)
+    {
+        if (s[i] != s[len] && a == 1 && b == 1)
+            return false;
+        if (s[i] != s[len] && ++a)
+            len++;
+    }
+    return (true);
+}
