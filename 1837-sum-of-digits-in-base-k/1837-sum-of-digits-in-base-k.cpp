1e68ec079bf1ab68c6cbff661beb44d257a6ff79
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-03-01 00:03:27 +0100

    Time: 3 ms (38.60%), Space: 6.1 MB (17.20%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int sumBase(int n, int k) {
+        int sum = 0;
         vector<int> res;
         while(n > 0)
         {
             res.push_back(n%k);
             n /= k;
         }
-        for(int i = 0; i< res.size()-1;i++)
-            res[i+1] += res[i];
-        return res.back();
+        for(int i = 0; i< res.size();i++)
+            sum += res[i];
+        return sum;
     }
 };
