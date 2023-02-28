53305f5343d9f0715328a949daae9b66a2903753
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-02-28 21:10:08 +0100

    Time: 4 ms (75.44%), Space: 10.1 MB (56.45%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i < nums.size()-1; i +=2)
+            for(int a = 0; a < nums[i] ; a++)
+                res.push_back(nums[i+1]);
+        return res;
+            
+    }
+};
