a9f49d7c60da7c30142959e9862125189b1950d8
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-03-02 06:19:24 +0100

    Time: 4 ms (28.90%), Space: 6.1 MB (43.35%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.cpp
+++ 1507-reformat-date/1507-reformat-date.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string reformatDate(string date) {
-        unordered_map <string,string> month = {{"Jan","01"}, {"Feb","02"}, {"Mar","03"}, {"Apr","04"}, {"May","05"}, {"Jun","06"},
+        map <string,string> month = {{"Jan","01"}, {"Feb","02"}, {"Mar","03"}, {"Apr","04"}, {"May","05"}, {"Jun","06"},
                                      {"Jul","07"}, {"Aug","08"}, {"Sep","09"}, {"Oct","10"}, {"Nov","11"}, {"Dec","12"}};
         string format;
         int len = date.find_last_of(" ");
