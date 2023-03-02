f2b892b7bacfbb0719437f5c5d2f4f3c04844371
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-03-02 06:19:52 +0100

    Time: 0 ms (100.00%), Space: 6 MB (57.03%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.cpp
+++ 1507-reformat-date/1507-reformat-date.cpp
@@ -1,14 +1,20 @@
-class Solution {
+ class Solution {
 public:
+    string ft_month(vector<string> &month, string m)
+    {
+        for(int i = 0; i < month.size() ; i++)
+            if (month[i] == m)
+                return (i>= 9) ? to_string(i+1) : ("0"+to_string(i+1));
+        return "";
+    }
     string reformatDate(string date) {
-        map <string,string> month = {{"Jan","01"}, {"Feb","02"}, {"Mar","03"}, {"Apr","04"}, {"May","05"}, {"Jun","06"},
-                                     {"Jul","07"}, {"Aug","08"}, {"Sep","09"}, {"Oct","10"}, {"Nov","11"}, {"Dec","12"}};
+        vector<string> month = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
         string format;
         int len = date.find_last_of(" ");
         format = date.substr(len+1,4);
         date[len] = ';';
         len = date.find_last_of(" ");
-        format += "-" + month[date.substr(len+1,3)];
+        format += "-" + ft_month(month, date.substr(len+1,3));
         date[len] = ';';
         string day = date.substr(0,len);
         format += (day.size() > 3) ? ("-" + day.substr(0,2)) :("-0" + day.substr(0,1));
