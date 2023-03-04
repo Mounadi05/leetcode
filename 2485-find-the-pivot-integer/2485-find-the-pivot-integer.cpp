3a02ef383f06083cddcb7f7ebeb0007968d8f070
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-03-04 23:52:25 +0100

    Time: 2 ms (58.33%), Space: 5.9 MB (61.47%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int pivotInteger(int n) {
         int left = 0;
-        if (n == 1) return n;
-        for(int i = 1; i < n ; i++)
+        int right = 0;
+        for(int i = 1; i <= n ; i++)
             left += i;
-        int right = n + n - 1;
-        for(int i = n - 2; i > 0 ; i--)
+        for(int i = n ; i > 0 ; i--)
         {
-            if (right == left) return i+1;
             right += i;
-            left -= i+1;
+            if (right == left) return i;
+            left -= i;
         }
         return -1;
     }
