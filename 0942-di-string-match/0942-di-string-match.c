b1ee46a770f6e9f65b69ce6039be74368525f2fd
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-03-06 03:42:43 +0100

    Time: 53 ms (32.50%), Space: 11.9 MB (5.00%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.c
@@ -0,0 +1,14 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* diStringMatch(char * s, int* returnSize){
+    int *str = malloc((strlen(s) + 1) * 4);
+    *returnSize = strlen(s)+1;
+    int strat = 0;
+    int end = strlen(s);
+    for(int i = 0; i < *returnSize;i++)
+        if (s[i] == 'I') str[i] = strat++;
+        else str[i] = end--;
+    return str;
+    
+}
