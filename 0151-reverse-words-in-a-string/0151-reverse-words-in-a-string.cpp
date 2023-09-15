3ef15c13511494c1dea2d53030cfb193c7bbfcaf
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-15 19:49:17 +0100

    Time: 8 ms (32.89%), Space: 8.9 MB (40.29%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,15 +4,16 @@ class Solution {
 public:
     string reverseWords(string s) {
        
-        string result;
-        std::regex r("\\S+");
-        auto begin = std::sregex_iterator(s.begin(), s.end(), r);
-        std::sregex_iterator end;
+        string result, tocken;
+        stringstream inp(s);
         vector<string> word;
-        for(auto i = begin ; i != end ; i++)
-          word.push_back(i->str());
+    
+        while(inp >> tocken)
+            word.push_back(tocken);
+
         for(auto i = word.rbegin() ; i != word.rend() ; i++)
             result += *i + " ";
-        return result.substr(0,result.length()-1);
+        
+        return result.substr(0, result.length()-1);
     }
 };
