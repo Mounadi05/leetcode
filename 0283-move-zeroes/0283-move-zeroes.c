d1ebbbf6cdab5b1771a4e6ea84530d3e267122b1
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-16 02:07:16 +0100

    Time: 69 ms (84.43%), Space: 15.9 MB (10.45%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,13 @@
+
+
+void moveZeroes(int* arr, int size){
+    int i = -1;
+    int j = 0;
+    while (++i < size)
+        if (arr[i])
+        {
+            arr[j++] = arr[i];
+            if (i >= j)
+                arr[i] = 0;
+        }
+}
