a54652615b3206c7d7f001bd3742c32a66e9956d
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-16 04:09:01 +0100

    Time: 65 ms (90.78%), Space: 62 MB (25.62%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -3,8 +3,7 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-    let i = Number.MAX_VALUE;
-    let j = Number.MAX_VALUE;
+    let i = Number.MAX_VALUE, j = Number.MAX_VALUE;
     for(const k of nums)
         if (k <= i) i = k;
         else if (k <= j) j = k;
