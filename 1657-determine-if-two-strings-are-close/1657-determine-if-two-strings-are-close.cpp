453b2c99c69cf710749911cebdba85459244b42f
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-18 01:45:47 +0100

    Time: 148 ms (17.75%), Space: 21.1 MB (7.24%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool closeStrings(string word1, string word2) {
         int chars1[26] = {0},chars2[26] = {0};
-        unordered_set<char> set1(word1.begin(), word1.end());
-        unordered_set<char> set2(word2.begin(), word2.end());
+        set<char> set1(word1.begin(), word1.end());
+        set<char> set2(word2.begin(), word2.end());
         for(char c : word1) chars1[c- 'a']++;
         for(char c : word2) chars2[c- 'a']++;
         sort(begin(chars1),end(chars1));
