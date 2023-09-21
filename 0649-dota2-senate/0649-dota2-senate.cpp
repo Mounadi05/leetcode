b588534995274360fca1aa82b4b2c51a89556139
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-21 04:54:16 +0100

    Time: 4 ms (85.14%), Space: 7.4 MB (70.62%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,32 +1,27 @@
  class Solution {
 public:
   string predictPartyVictory(string senate) {
-        vector<string> vote;
         int R = 0, D = 0,skipRadiant = 0, skipDire= 0;
-        for(char c : senate)
-        { 
+        
+        for(char c : senate){ 
             if (c == 'R')R++;
             else D++;
         }
-        for(int i = 0; i < senate.size() && R > 0 && D > 0;i++)
-        {
+      
+        for(int i = 0; i < senate.size() && R > 0 && D > 0;i++){
            
-            if (senate[i] == 'R' && skipRadiant < 0){
-                skipRadiant++;
-                continue;
-            }
-            if (senate[i] == 'D'  && skipDire < 0){
-                skipDire++;
-                continue;
-            }
-            if (senate[i] == 'R')
+            if ((senate[i] == 'R' && skipRadiant < 0) || (senate[i] == 'D'  && skipDire < 0))
             {
+                skipRadiant += (senate[i] == 'R');
+                skipDire += senate[i] == 'D';
+                continue;
+            }
+            if (senate[i] == 'R'){
                 skipDire--;
                 D--;
                 senate.push_back('R');
             }
-            else 
-            {
+            else {
                 skipRadiant--;
                 R--;
                 senate.push_back('D');
