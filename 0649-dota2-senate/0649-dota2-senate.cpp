4c6525c09768d152e7de0036515c8653ede2cfa9
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-21 21:35:32 +0100

    Time: 7 ms (53.36%), Space: 7.4 MB (70.81%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,12 +1,32 @@
-class Solution {
+  class Solution {
 public:
-  string predictPartyVictory(string s) {
-        int R = 0, D = 0,sR = 0, sD= 0;
-        for(char c : s)(c == 'R') ? R++:D++;
-        for(int i = 0; i < s.size() && R > 0 && D > 0;i++){
-            if ((s[i] == 'R' && sR < 0) || (s[i] == 'D'&& sD < 0)){sR += (s[i] == 'R'),sD += s[i] == 'D';continue;}
-            if (s[i] == 'R')sD--,D--,s.push_back('R');
-            else sR--,R--,s.push_back('D');
+  string predictPartyVictory(string senate) {
+        int R = 0, D = 0,skipRadiant = 0, skipDire= 0;
+        
+        for(char c : senate){ 
+            if (c == 'R')R++;
+            else D++;
+        }
+      
+        for(int i = 0; i < senate.size() && R > 0 && D > 0;i++){
+           
+            if ((senate[i] == 'R' && skipRadiant < 0) || (senate[i] == 'D'  && skipDire < 0))
+            {
+                skipRadiant += (senate[i] == 'R');
+                skipDire += senate[i] == 'D';
+                continue;
+            }
+            if (senate[i] == 'R'){
+                skipDire--;
+                D--;
+                senate.push_back('R');
+            }
+            else {
+                skipRadiant--;
+                R--;
+                senate.push_back('D');
+            }
+         
          }
          return (R < D) ? "Dire" : "Radiant";
     }
