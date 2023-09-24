e7a0449298d4a0b5851690ebbd72263c5465a5d0
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-24 23:32:20 +0100

    Time: 31 ms (20.00%), Space: 26.8 MB (5.71%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.c
@@ -0,0 +1,31 @@
+ bool check(char * s, char * p,int i,int j,int **memo)
+{
+    if (memo[i][j]) return false;
+    memo[i][j] = 1;
+    if (!s[i] && !p[j]) return true;
+    if (p[j] &&  p[j] == '*' && !p[j + 1] && !s[i]) return true;
+    if(s[i] && p[j] && (s[i]  == p[j] || (p[j] && p[j] == '?'))) return !memo[i+1][j+1] && check(s,p,i+1,j+1,memo);
+    if(p[j] == '*') 
+         return (s[i] && !memo[i+1][j] && check(s,p,i+1,j,memo) )||( !memo[i][j+1] && check(s,p,i,j+1,memo));
+    return  false;
+}
+bool isMatch(char * s, char * p)
+{
+    int m = strlen(s);
+    int n = strlen(p);
+
+    int **memo = (int **)malloc((m + 1) * sizeof(int *));
+    for (int i = 0; i <= m; i++) {
+        memo[i] = (int *)calloc(n + 1, sizeof(int));
+    }
+
+    bool result = check(s, p, 0, 0, memo);
+
+    for (int i = 0; i <= m; i++) {
+        free(memo[i]);
+    }
+    free(memo);
+
+    return result;
+} 
+  
