da8ddeabb8494d35f6d731bb8be3b609c126424d
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-25 02:37:09 +0100

    Time: 32 ms (44.44%), Space: 45.3 MB (5.04%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.cpp
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
+         ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector<string> result;
         for(string word : words)
         {
             stringstream ss(word);
             string w;
-            while (!ss.eof()) 
+            while(!ss.eof()) 
             {
                 getline(ss, w, separator);
                 if (!w.empty()) result.push_back(w);
