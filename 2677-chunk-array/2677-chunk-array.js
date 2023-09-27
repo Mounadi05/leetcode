127bf47d409f7a312611e4034b84c19d8ba2067c
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-27 22:31:00 +0100

    Time: 51 ms (81.07%), Space: 44.1 MB (83.84%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -4,12 +4,9 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-        //console.log(arr.splice(0,1));
-
+ 
     var result = [];
     for(let i = 0 ; i < arr.length;i +=size)
-    {
          result.push(arr.slice(i,size+i));
-    }
      return result;
 };
