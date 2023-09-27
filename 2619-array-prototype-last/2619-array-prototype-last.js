f427dfade1a12222f339dcd44e4e0781426fabde
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-27 22:12:14 +0100

    Time: 56 ms (16.89%), Space: 41.5 MB (66.26%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,9 +2,8 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    var arr = [...this];
-   if(arr.length == 0) return -1;
-   return this[this.length-1]; 
+   if (this.length === 0) return -1;
+      return this[this.length - 1];
 };
 
 /**
