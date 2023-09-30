ce7cc2b4c2b9838cd3c5b0c28ef3ef61f2a33272
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-30 01:59:01 +0100

    Time: 8 ms (13.51%), Space: 12.5 MB (95.81%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -11,24 +11,52 @@
  */
 class Solution {
 public:
-   void countPath(TreeNode* root,vector<string> &result, string path)
-    {
-        if (!root) return;
-        if (!root->right && !root->left)
-        {
-            path += to_string(root->val);
-            result.push_back(path);
-            path.clear();
-            return;
-        }
-        path += to_string(root->val) + "->";
-        countPath(root->right,result,path);
-        countPath(root->left,result,path);
-    }
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> result;
-        countPath(root,result,"");
+//    void countPath(TreeNode* root,vector<string> &result, string path)
+//     {
+//         if (!root) return;
+//         if (!root->right && !root->left)
+//         {
+//             path += to_string(root->val);
+//             result.push_back(path);
+//             path.clear();
+//             return;
+//         }
+//         path += to_string(root->val) + "->";
+//         countPath(root->right,result,path);
+//         countPath(root->left,result,path);
+//     }
+//     vector<string> binaryTreePaths(TreeNode* root) {
+//         vector<string> result;
+//         countPath(root,result,"");
       
+//         return result;
+//     }
+        
+    vector<string> binaryTreePaths(TreeNode* root) 
+    {
+        stack<TreeNode*> tree;
+        tree.push(root);
+        stack<string> paths;
+        paths.push(to_string(root->val));
+        vector<string> result;
+        while(!tree.empty())
+        {
+            TreeNode* current = tree.top();
+            string path = paths.top();
+            tree.pop();
+            paths.pop();
+            if (!current->left && !current->right)  
+                 result.push_back(path);
+      
+            if (current->right) {
+                paths.push(path + "->" + to_string(current->right->val));
+                tree.push(current->right);
+            }
+            if (current->left) {
+                paths.push(path + "->" + to_string(current->left->val));
+                tree.push(current->left);
+            }
+        }
         return result;
     }
 };
