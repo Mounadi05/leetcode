b77f54309cd090d9f743adda814adea0cad9d298
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-09-30 05:01:01 +0100

    Time: 2076 ms (13.57%), Space: 524.9 MB (5.55%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,25 +11,35 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-       queue<TreeNode*> tree;
-        vector<vector<int>> result;
-        if(root) tree.push(root);
-        int levels = 0;
-        while(!tree.empty())
-        {
-            levels = tree.size();
-            vector<int> level;
-            for(int i = 0; i < levels;i++)
-            {
-                TreeNode* current = tree.front();
-                tree.pop();
-                level.push_back(current->val);
-                if(current->left) tree.push(current->left);
-                if(current->right) tree.push(current->right);
-            }
-            result.push_back(level);
-        }
-        return result;
+    // vector<vector<int>> levelOrder(TreeNode* root) {
+    //    queue<TreeNode*> tree;
+    //     vector<vector<int>> result;
+    //     if(root) tree.push(root);
+    //     int levels = 0;
+    //     while(!tree.empty())
+    //     {
+    //         levels = tree.size();
+    //         vector<int> level;
+    //         for(int i = 0; i < levels;i++)
+    //         {
+    //             TreeNode* current = tree.front();
+    //             tree.pop();
+    //             level.push_back(current->val);
+    //             if(current->left) tree.push(current->left);
+    //             if(current->right) tree.push(current->right);
+    //         }
+    //         result.push_back(level);
+    //     }
+    //     return result;
+    // }
+    vector<vector<int>> res;
+    vector<vector<int>> levelOrder(TreeNode* root, int level = 0) {
+        if (!root) return res;
+        if (res.size() > level) res[level].push_back(root->val);
+        else res.push_back({root->val});
+        levelOrder(root->left, level + 1);
+        levelOrder(root->right, level + 1);
+        return res;
     }
+
 };
