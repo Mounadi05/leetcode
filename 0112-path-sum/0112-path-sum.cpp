d1c4e05581aa6c938614855a31c61468413acce3
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-01 04:57:12 +0100

    Time: 11 ms (30.99%), Space: 21.8 MB (9.62%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -12,10 +12,26 @@
 class Solution {
 public:
     
-    bool hasPathSum(TreeNode* root, int targetSum,int sum = 0) {
-        if (!root) return false;
-        sum += root->val; 
-        if (!root->right && !root->left) return (targetSum == sum);
-        return hasPathSum(root->left,targetSum,sum) || hasPathSum(root->right,targetSum,sum);
+    // bool hasPathSum(TreeNode* root, int targetSum,int sum = 0) {
+    //     if (!root) return false;
+    //     sum += root->val; 
+    //     if (!root->right && !root->left) return (targetSum == sum);
+    //     return hasPathSum(root->left,targetSum,sum) || hasPathSum(root->right,targetSum,sum);
+    // }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        stack<TreeNode*> tree;
+        if(!root) return false;
+        tree.push(root);
+        while(!tree.empty())
+        {
+            TreeNode *current = tree.top();
+            tree.pop();
+            if(!current->right && !current->left && current->val == targetSum)
+                return true;
+            if(current->left) current->left->val += current->val, tree.push(current->left);
+            if(current->right) current->right->val += current->val, tree.push(current->right);
+        }
+        return false;
     }
 };
