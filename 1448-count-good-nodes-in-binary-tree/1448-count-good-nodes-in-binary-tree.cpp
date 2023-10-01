a879c4752fa5dcc4bf61d9f610bfb8e50167f582
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-01 05:49:36 +0100

    Time: 114 ms (17.13%), Space: 86.8 MB (9.29%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,21 +11,45 @@
  */
 class Solution {
 public:
-     int countNodes(TreeNode* node, int max_val) {
-        if (node == nullptr)
-            return 0;
+//      int countNodes(TreeNode* node, int max_val) {
+//         if (!node) return 0;
 
-        int count = (node->val >= max_val) ? 1 : 0;
+//         int count = (node->val >= max_val) ? 1 : 0;
 
-        max_val = max(max_val, node->val);
+//         max_val = max(max_val, node->val);
 
-        count += countNodes(node->left, max_val);
-        count += countNodes(node->right, max_val);
+//         count += countNodes(node->left, max_val);
+//         count += countNodes(node->right, max_val);
 
+//         return count;
+//     }
+//     int goodNodes(TreeNode* root) {
+//         return countNodes(root, root->val);
+
+//     }
+     int goodNodes(TreeNode* root) {
+        stack<TreeNode*> tree;
+        stack<int> maxValue;
+        tree.push(root);
+        maxValue.push(INT_MIN);
+        int count = 0;
+        while(!tree.empty())
+        {
+            TreeNode* current = tree.top();
+            int value = maxValue.top();
+            tree.pop();
+            maxValue.pop();
+            count += (current->val >= value);
+            if(current->left) {
+                maxValue.push(max(value, current->val));
+                tree.push(current->left);
+            }
+            if(current->right) {
+                maxValue.push(max(value, current->val));
+                tree.push(current->right);
+            }
+        }
         return count;
-    }
-    int goodNodes(TreeNode* root) {
-        return countNodes(root, root->val);
 
     }
 };
