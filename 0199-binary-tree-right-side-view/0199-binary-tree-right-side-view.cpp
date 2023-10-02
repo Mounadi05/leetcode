90d9a1fbbeddbcd858fd71e61065f3f3471fc08d
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-02 04:26:28 +0100

    Time: 0 ms (100.00%), Space: 12.5 MB (7.51%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,16 +11,40 @@
  */
 class Solution {
 public:
-    vector<int> result;
-    vector<int> rightSideView(TreeNode* root, int index = 0) {
-        if (!root) return {};
+//     vector<int> result;
+//     vector<int> rightSideView(TreeNode* root, int index = 0) {
+//         if (!root) return {};
         
-        if (result.size() <= index) result.push_back(root->val);
-        else result[index] = root->val;
+//         if (result.size() <= index) result.push_back(root->val);
+//         else result[index] = root->val;
 
-        rightSideView(root->left, index + 1);
-        rightSideView(root->right, index + 1);
+//         rightSideView(root->left, index + 1);
+//         rightSideView(root->right, index + 1);
 
-        return result;
-    }
+//         return result;
+//     }
+     vector<int> rightSideView(TreeNode* root, int index = 0)
+     {
+         queue<TreeNode*> nodes;
+         if (!root) return {};
+         nodes.push(root);
+         vector<int> result;
+         int level = 0;
+         while(!nodes.empty())
+         {
+             int size(nodes.size());
+             for(int i = 0; i < size ;i++)
+             {
+                 TreeNode * current = nodes.front();
+                 nodes.pop();
+                 if (result.size() <= level) result.push_back(current->val);
+                 else result[level] = current->val;
+                 if(current->left) nodes.push(current->left);
+                 if(current->right) nodes.push(current->right);
+             }
+             level++;
+
+         }
+         return result;
+     }
 };
