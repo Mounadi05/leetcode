3f2eda380be5ee674ca150f18157d5011d0ca7f8
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-05 03:37:58 +0100

    Time: 84 ms (7.44%), Space: 16.2 MB (18.73%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> result;
+        if (nums.size() == 1) return {nums[0]};
+        pair<int , int>a = {0,0};
+        pair<int , int>b = {0,0};
+        for(int num : nums)
+        {
+           //cout <<"before : "<< a.first << " " << a.second << " | " << b.first << " " << b.second <<endl;
+
+            if(num == a.first) a.second++;
+            else if(num == b.first) b.second++;
+            else if (!a.second ) a = {num,1}; 
+            else if (!b.second) b = {num,1};
+            else
+                 b.second--,a.second--;
+          cout << "after : "<< a.first << " " << a.second << " | " << b.first << " " << b.second <<endl;
+
+        }
+            
+        a.second = b.second = 0;
+        for (int num : nums)
+        {
+            a.second +=(a.first == num);
+            b.second +=(b.first == num);
+        }
+        if (a.second > nums.size()/3 ) result.push_back(a.first);
+        if (b.second > nums.size()/3 && a.first != b.first ) result.push_back(b.first);
+        return result;
+    }
+};
