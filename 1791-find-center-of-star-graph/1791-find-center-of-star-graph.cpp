2693fa08337d16451435ea9ac20b29135febd9e8
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-05 01:07:19 +0100

    Time: 149 ms (81.46%), Space: 67.7 MB (53.47%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& edges) {
-        map<int, vector<int>> stars;
-        for(auto edge: edges)
-        {
-            stars[edge[0]].push_back(edge[1]);
-            stars[edge[1]].push_back(edge[0]);
-        }
-        pair<int,int>center = {0,-1};
-        for(auto star: stars)
-        {
-            if(star.second.size() > center.first)
-            {
-                center.first = star.first;
-                center.second = star.second.size();
-            }
-        }
-        return center.first;
-    }
+    // int findCenter(vector<vector<int>>& edges) {
+    //     map<int, vector<int>> stars;
+    //     for(auto edge: edges)
+    //     {
+    //         stars[edge[0]].push_back(edge[1]);
+    //         stars[edge[1]].push_back(edge[0]);
+    //     }
+    //     pair<int,int>center = {0,-1};
+    //     for(auto star: stars)
+    //     {
+    //         if(star.second.size() > center.first)
+    //         {
+    //             center.first = star.first;
+    //             center.second = star.second.size();
+    //         }
+    //     }
+    //     return center.first;
+    // }
+      int findCenter(vector<vector<int>>& edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+      }
+
+    
 };
