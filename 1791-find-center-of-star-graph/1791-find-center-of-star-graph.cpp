2f532cf64a5269121f9bf0d7622fbf7bf73219eb
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-05 01:20:36 +0100

    Time: 157 ms (61.22%), Space: 67.5 MB (58.12%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -19,7 +19,7 @@ public:
     //     return center.first;
     // }
       int findCenter(vector<vector<int>>& edges) {
-        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+        return edges[0][edges[0][1] == edges[1][0] || edges[0][1] == edges[1][1]];
       }
 
     
