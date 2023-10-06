6a399c42c0c788b30bca75e0b2950ed45d4ded43
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-06 01:41:16 +0100

    Time: 28 ms (34.44%), Space: 71.7 MB (19.10%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,71 +14,34 @@ public:
         ListNode * tmp =  new ListNode();
         ListNode *result = tmp;
         int rst = 0;
-        while(l1 && l2)
+        int sum  = 0;
+        while(l1 || l2 || rst)
         {
-            if (rst + l1->val + l2->val < 10)
+            sum = rst,rst = 0;
+            if (l1)
             {
-                tmp->val =(rst + l1->val + l2->val);
-                rst  = 0;
+                sum += l1->val;
+                rst = 0;
+                l1 = l1->next;
             }
-            else
+            if(l2) 
             {
-                tmp->val = (rst + l1->val + l2->val)%10;
-                rst = (rst + l1->val + l2->val)/10;
+                sum += l2->val;
+                l2 = l2->next;
             }
-            l1 = l1->next;
-            l2 = l2->next;
+            if (sum > 9)
+            {
+                int carry = sum;
+                sum = carry % 10;
+                rst = carry / 10; 
+            }
+            tmp->val = sum;
             if (rst || l2 || l1)
             {
                 tmp->next = new ListNode();
                 tmp = tmp->next;
             }
         }
-        if(l1)
-        {
-            while(l1)
-            {
-                if (rst + l1->val < 10)
-                {
-                    tmp->val =(rst + l1->val);
-                    rst  = 0;
-                }
-                else
-                {
-                    tmp->val = (rst + l1->val)%10;
-                    rst = (rst + l1->val)/10;
-                }
-                l1 = l1->next;
-                if (rst || l2 || l1)
-                {
-                    tmp->next = new ListNode();
-                    tmp = tmp->next;
-                }
-            }
-        }
-        if(l2)
-        {
-            while(l2)
-            {
-                if (rst + l2->val < 10)
-                {
-                    tmp->val =(rst + l2->val);
-                    rst = 0;
-                }
-                else
-                {
-                    tmp->val = (rst + l2->val)%10;
-                    rst = (rst + l2->val + l2->val)/10;
-                }
-                l2 = l2->next;
-                if (rst || l2)
-                {
-                    tmp->next = new ListNode();
-                    tmp = tmp->next;
-                }
-            }
-        }
-        if (rst) tmp->val =rst;
         return result;
     }
 };
