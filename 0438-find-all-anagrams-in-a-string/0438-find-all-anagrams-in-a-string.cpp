e7d38858e01ac600eb0db37a3b428f7552081900
Mounadi05 <49256019+Mounadi05@users.noreply.github.com>
2023-10-06 06:03:54 +0100

    Time: 7 ms (92.72%), Space: 9.1 MB (35.49%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -5,15 +5,14 @@ public:
         vector<int> hash(26,-1);
         vector<int> tmp(26,-1);
         int index = 0;
-        for(char c : p)
-            hash[c - 'a']++;
-        int n = p.size();
+        for(char c : p) hash[c - 'a']++;
+        int n = p.size() - 1;
         for (int i = 0; i < s.size(); i++) 
         {
             tmp[s[i] - 'a']++;
-            if (i >= n - 1) 
+            if (i >= n) 
             {
-                if (tmp == hash) result.push_back(i - n + 1);
+                if (tmp == hash) result.push_back(i - n);
                 tmp[s[index++] - 'a']--;
             }
         }
